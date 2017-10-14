33a419947e91dde71e109ad8c8a69aa3c3a356f3
coolcat-creations <mail@coolcat-creations.com>
2017-10-14 11:53:17 +0200

    initial commit

---
+++ mod_ccctwoclick/media/js/script.js
@@ -0,0 +1,66 @@
+/**
+ * @package     Joomla.Module
+ * @subpackage  Module.Ccctwoclick
+ *
+ * @copyright   Copyright (C) 2017 COOLCAT creations. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+(function() {
+
+	document.addEventListener('DOMContentLoaded', function() {
+
+		(function() {
+
+			var ccctwoclickcontainer = document.querySelectorAll( ".ccctwoclickcontainer" );
+
+			for (var i = 0; i < ccctwoclickcontainer.length; i++) {
+
+				var content = ccctwoclickcontainer[i].querySelectorAll( ".ccctwoclick" );
+				var enablebtn = ccctwoclickcontainer[i].querySelectorAll( ".ccctwoclickreveal" );
+				var disablebtn = ccctwoclickcontainer[i].querySelectorAll(".ccctwoclickdisable");
+				var contentbefore = ccctwoclickcontainer[i].querySelectorAll(".contentbefore");
+
+				enablebtn[0].addEventListener( "click", function() {
+
+					var iframe = document.createElement( "iframe" );
+
+					iframe.setAttribute( 'frameborder', '0' );
+					iframe.setAttribute( 'allowfullscreen', 'true' );
+					iframe.setAttribute( 'allowtransparency', 'true' );
+					iframe.setAttribute( 'scrolling', 'no' );
+					iframe.setAttribute( 'title', 'fb:page Facebook Social Plugin');
+
+					iframe.setAttribute( 'name', 'f2f966e5973af' );
+					iframe.setAttribute( 'width', content[0].dataset.width );
+					iframe.setAttribute( 'height', content[0].dataset.height );
+
+					iframe.setAttribute( 'src', content[0].dataset.source );
+
+					content[0].innerHTML = "";
+					content[0].appendChild( iframe );
+					content[0].style.background = null;
+					content[0].style.backgroundSize = null;
+
+					enablebtn[0].style.display = 'none';
+					contentbefore[0].style.display = 'none';
+
+					disablebtn[0].style.display = 'inline';
+					disablebtn[0].classList.toggle('disablecontent');
+
+				} );
+
+				disablebtn[0].addEventListener( "click", function() {
+					content[0].innerHTML = "";
+					disablebtn[0].style.display = 'none';
+					enablebtn[0].style.display = 'inline';
+					contentbefore[0].style.display = 'block';
+
+				} );
+
+			};
+
+		} )();
+	});
+
+})();
---
+++ mod_ccctwoclick/mod_ccctwoclick.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Joomla.Module
+ * @subpackage  Module.Ccctwoclick
+ *
+ * @copyright   Copyright (C) 2017 COOLCAT creations. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die();
+
+$iwidth = $params->get('iframewidth', '800');
+$iheight = $params->get('iframeheight', '600');
+$frameborder = $params->get('frameborder', '0');
+$allowfullscreen = $params->get('allowfullscreen', '');
+$allowtransparency = $params->get('allowtransparency', '');
+$scrolling = $params->get('scrolling', '');
+$allowtransparency = $params->get('allowtransparency', '');
+$isrc = $params->get('sourceurl', '');
+$contentBefore = $params->get('contentbeforereplacement', '');
+$btntxtReveal = $params->get('buttontxtreveal', 'MOD_CCCTWOCLICK_BTN_REVEAL_TEXT');
+$btntxtDisable = $params->get('buttontxtdisable', 'MOD_CCCTWOCLICK_BTN_DISABLE_TEXT');
+$btnclassDisable = $params->get('buttontxtrevealclass', '');
+$btnclassEnable = $params->get('buttontxtdisableclass', '');
+$disabledimage = $params->get('disabledimage', '');
+$backgroundsize = $params->get('backgroundsize', '');
+$stylesheet = $params->get('stylesheet', "yes");
+
+require JModuleHelper::getLayoutPath('mod_ccctwoclick', $params->get('layout', 'default'));
